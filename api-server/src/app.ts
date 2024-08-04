a53b1dcfb7cb15658c223d6c443b97705acd8d83
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2024-08-05 01:37:03 +0530

    feat: cors issue

--- api-server/src/app.ts
+++ api-server/src/app.ts
@@ -3,10 +3,13 @@ import dotenv from 'dotenv';
 import indexRouter from './routes/index';
 import errorHandler from './middlewares/errorHandler';
 import logger from './utils/logger';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
     logger.info(`${req.method} ${req.url}`);
