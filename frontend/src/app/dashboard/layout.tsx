4b25fa39b58972e5af9e01127a83e53cd9bcb5ac
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2024-08-06 15:43:43 +0530

    fix: css issue

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -2,9 +2,7 @@ import { Inter } from "next/font/google";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/shared/theme-provider";
-import { Toaster } from "@/components/ui/toaster"
-
-import "../../app/globals.css";
+import { Toaster } from "@/components/ui/toaster";
 import DashboardLayout from "@/components/shared/dashboard-layout";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,20 +12,17 @@ const fontSans = FontSans({
   variable: "--font-sans",
 });
 
-
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <DashboardLayout>{children}</DashboardLayout>
-          <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <DashboardLayout>{children}</DashboardLayout>
+        <Toaster />
+      </ThemeProvider>
+    </>
   );
 }
